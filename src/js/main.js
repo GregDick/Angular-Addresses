3a3df1bdc4bb1dc5751a952d44725675ef4b6c36
Greg Dick <gregory.j.dick@vanderbilt.edu>
2015-06-03 13:59:30 -0500

    added validation and a modal

--- src/js/main.js
+++ src/js/main.js
@@ -4,11 +4,11 @@ angular
   .controller('Main', function(){
     var vm = this;
     vm.people = [
-    {name: 'Earth', twitter: '@CaptainPlanet', number: '(123)555-1111'},
-    {name: 'Greg', twitter: '@Greg', number: '(123)555-2222'},
-    {name: 'Shmoe', twitter: '@JoeShmoe', number: '(123)555-3333'},
-    {name: 'Alan', twitter: '@Rickman_Alan', number: '(123)867-5309'},
-    {name: "Bradley", twitter: '@B_rad', number: '(123)555-5555'}
+    {name: 'Earth', twitter: '@CaptainPlanet', number: '(123)555-1111', photo:'http://i.imgur.com/fsw9I8V.gif'},
+    {name: 'Greg', twitter: '@Greg', number: '(123)555-2222', photo:'http://i.imgur.com/uTONfVS.jpg'},
+    {name: 'Shmoe', twitter: '@JoeShmoe', number: '(123)555-3333', photo:'http://i.imgur.com/yLCfE5l.jpg'},
+    {name: 'Alan', twitter: '@Rickman_Alan', number: '(123)867-5309', photo:'http://i.imgur.com/AAuJNDl.jpg'},
+    {name: "Bradley", twitter: '@B_rad', number: '(123)555-5555', photo:'http://i.imgur.com/Vo0pllf.jpg'}
     ];
 
     vm.newPerson = {};
