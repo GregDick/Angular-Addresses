bd9a88c240db503400c339c939bdc7a5e4519f0f
Greg Dick <gregory.j.dick@vanderbilt.edu>
2015-08-22 13:34:31 -0500

    updated file structre

---
+++ src/assets/main.constants.js
@@ -0,0 +1,5 @@
+angular
+  //without second argument, module just looks up an existing module as opposed to creating a new one
+  .module('angularAddresses')
+
+  .constant('API_URL', 'https://angleadresses.firebaseio.com');
---
+++ src/assets/main.run.js
@@ -0,0 +1,9 @@
+angular
+  .module('angularAddresses')
+  .run(function ($rootScope, Auth) {
+    $rootScope.$on('$routeChangeStart', function (event, nextRoute) {
+      if (nextRoute.$$route && nextRoute.$$route.private) {
+        Auth.requireLogin();
+      }
+    });
+  });
